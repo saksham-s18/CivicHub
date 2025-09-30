ae4efa16fa0a9ddee3afa002443fead128080519
Saksham-Singh18 <thakursakshamsingh3@gmail.com>
2025-09-30 16:34:28 +0530

    refactor: Secure database credentials using .env file

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
 # database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+load_dotenv()
 
-# Replace with your actual database credentials
-# The corrected line
-DATABASE_URL = "postgresql://postgres:Saksham%4018@localhost/civichub"
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
